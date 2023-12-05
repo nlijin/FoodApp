e0799dcec59d5f13da84ba04997f8d4ee9d5cf17
nlijin <nlijinraj@gmail.com>
2023-12-05 23:46:13 +0530

    few ui fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const REST_IMAGES =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const RESTAURANTS_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9879659&lng=77.6895248&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9261243&lng=77.60707029999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9879659&lng=77.6895248&restaurantId=";
